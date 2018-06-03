fa9efde668877030a5737c608c6c26987f8221d4
lalalacxx <2420059729@qq.com>
2018-06-03 07:26:15 -0700

    头文件

---
+++ Sort/sort.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+//冒泡排序
+void BubbleSort(int arr[],int len);
+//选择排序
+void SelectSort(int arr[],int len);
+//插入排序
+void InsertSort(int arr[],int len);
+//堆排序
+void HeapSort(int arr[],int len);
+//希尔排序
+void ShellSort(int arr[],int len);
+//归并排序递归版本
+void MergeSort(int arr[],int len);
+//归并排序非递归版本
+void MergeSortByLoop(int arr[],int len);
+//快速排序递归版本
+void QuickSort(int arr[],int len);
+//快速排序非递归版本
+void QuickSortByLoop(int arr[],int len);
